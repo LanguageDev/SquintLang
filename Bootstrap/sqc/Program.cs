f2b51f790ea94767bc8b73d93e493fc936b645e4
Lenkefi Péter <lenkefi.peti@gmail.com>
2022-06-17 14:32:33 +0200

    Update Program.cs

--- Bootstrap/sqc/Program.cs
+++ Bootstrap/sqc/Program.cs
@@ -8,15 +8,6 @@ namespace Squint.Compiler;
 
 internal class Program
 {
-    private static IParseTree ParseSyntaxTree(string text)
-    {
-        var inputStream = new AntlrInputStream(text);
-        var lexer = new SquintLexer(inputStream);
-        var commonTokenStream = new CommonTokenStream(lexer);
-        var parser = new SquintParser(commonTokenStream);
-        return parser.file();
-    }
-
     private static void Main(string[] args)
     {
         var text = File.ReadAllText("test.squint");
